81c4f4ad9d2f344245fd2ec98ea5227f588d6dd2
MCyangguang <342540005@qq.com>
2025-04-29 19:29:40 +0800

    Initial commit

---
+++ src/main/java/com/mcyangguang/horsecar/HorseCarModClient.java
@@ -0,0 +1,10 @@
+package com.mcyangguang.horsecar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HorseCarModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
